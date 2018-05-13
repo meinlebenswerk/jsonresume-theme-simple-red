661b4605fb1a516b8660abf229e1b63aa91fa2a1
meinlebenswerk <janmax99+qithub@gmail.com>
2018-05-13 17:39:16 +0200

    fixed a bug in education formatting

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var fs = require("fs");
 var path = require('path');
 var Handlebars = require("handlebars");
 var moment = require("moment");
+moment.locale('de');
 
 function render(resume) {
 	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
